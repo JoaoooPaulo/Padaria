b3222fd785f38c992d0d7588aae3d752004d8ceb
João Paulo Silvaa <149210077+JoaoooPaulo@users.noreply.github.com>
2023-11-08 22:11:11 -0300

    Initial commit

---
+++ Padaria/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Padaria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            //Instanciar a janela de login 
+
+            Login janela = new Login();
+
+            //mostrar janela:
+            janela.ShowDialog();
+
+            //fechar tudo ao fechar o login:
+            Application.Exit();
+        }
+    }
+}
