7cdc0d71be8d7097d34c929a13cb7ca381a90d98
João Paulo Silvaa <149210077+JoaoooPaulo@users.noreply.github.com>
2023-11-14 22:11:09 -0300

    Foi feito mais alguns ajustes e foi inciado mais um formulário para adesão de Produtos
    código código e mais códigos para Atualização e adaptação do Sistema.....

--- Padaria/Classes/Usuário.cs
+++ Padaria/Classes/Usuário.cs
@@ -28,6 +28,7 @@ namespace Padaria.Classes
             MySqlCommand cmd = new MySqlCommand(comando, con);
             // Substituir os 'coringas' por valores:
             cmd.Parameters.AddWithValue("@email", Email);
+
             // Obter o hash da senha:
             var hashsenha = EasyEncryption.SHA.ComputeSHA256Hash(Senha);
             cmd.Parameters.AddWithValue("@Senha", hashsenha);
@@ -55,6 +56,106 @@ namespace Padaria.Classes
             conexaoBD.Desconectar(con);
             return tabela;
         }
-    }   
-    
+        public bool Cadastrar()
+        {
+            string comando = "INSERT INTO usuarios (nome_completo, email, senha) " +
+                "VALUES (@nome_completo, @email, @senha)";
+            Banco.ConexaoBanco conexaoBD = new Banco.ConexaoBanco();
+            MySqlConnection con = conexaoBD.ObterConexao();
+            MySqlCommand cmd = new MySqlCommand(comando, con);
+            cmd.Parameters.AddWithValue("@nome_completo", NomeCompleto);
+            cmd.Parameters.AddWithValue("@email", Email);
+
+            //Obter o hash:
+            string hashsenha = EasyEncryption.SHA.ComputeSHA256Hash(Senha);
+            cmd.Parameters.AddWithValue("@senha", hashsenha);
+            cmd.Prepare();
+            try
+            {
+                if (cmd.ExecuteNonQuery() == 0)
+                {
+                    conexaoBD.Desconectar(con);
+                    return false;
+                }
+                else
+                {
+                    conexaoBD.Desconectar(con);
+                    return true;
+                }
+            }
+            catch
+            {
+                conexaoBD.Desconectar(con);
+                return false;
+            }
+        }
+
+        public bool Apagar()
+        {
+            string comando = "DELETE FROM usuarios WHERE id = @id";
+            Banco.ConexaoBanco conexaoBD = new Banco.ConexaoBanco();
+            MySqlConnection con = conexaoBD.ObterConexao();
+            MySqlCommand cmd = new MySqlCommand(comando, con);
+            cmd.Parameters.AddWithValue("@id", Id);
+            cmd.Prepare();
+
+            try
+            {
+                if (cmd.ExecuteNonQuery() == 0)
+                {
+                    conexaoBD.Desconectar(con);
+                    return false;
+                }
+                else
+                {
+                    conexaoBD.Desconectar(con);
+                    return true;
+                }
+            }
+            catch
+            {
+                conexaoBD.Desconectar(con);
+                return false;
+            }
+            public bool Editar();
+            {
+                string comando = "UPDATE usuarios SET nome_completo = @nome_completo, " +
+                    "email = @email, senha = @senha, WHERE id = @id";
+                //Comando caso a senha esteja vazia:
+                if(Senha == "")
+                {
+                    comando = "UPDATE usuarios SET nome_completo = @nome_completo, " +
+                    "email = @email, WHERE id = @id";
+                }
+                Banco.ConexaoBanco conexaoBD = new Banco.ConexaoBanco();
+                MySqlConnection con = conexaoBD.ObterConexao();
+                MySqlCommand cmd = new MySqlCommand(comando, con);
+                cmd.Parameters.AddWithValue("@nome_completo", NomeCompleto);
+                cmd.Parameters.AddWithValue("@email", Email);
+
+                //Obter o hash:
+                string hashsenha = EasyEncryption.SHA.ComputeSHA256Hash(Senha);
+                cmd.Parameters.AddWithValue("@senha", hashsenha);
+                cmd.Prepare();
+                try
+                {
+                    if (cmd.ExecuteNonQuery() == 0)
+                    {
+                        conexaoBD.Desconectar(con);
+                        return false;
+                    }
+                    else
+                    {
+                        conexaoBD.Desconectar(con);
+                        return true;
+                    }
+                }
+                catch
+                {
+                    conexaoBD.Desconectar(con);
+                    return false;
+                }
+            }
+        }
+    }       
 }
